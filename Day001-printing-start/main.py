b2a712ccf56466633b9c872cdae37e0ed6a0ceac
Bernat Real <bernat.real@hotelsviva.com>
2023-12-02 12:22:29 +0100

    Day 1

---
+++ Day001-printing-start/main.py
@@ -0,0 +1,15 @@
+# print("Hello world!")
+
+# print("Hello world!\nHello world!\nHello world!")
+
+# print("Hello" + " " + "Bernat")
+
+# execution goes from inside to outside,
+# then first it executes input and then print
+# print("Hello " + input("What is your name?\n"))
+
+# print(len(input()))
+
+name = input("Whats is your name?")
+length = len(name)
+print(length)
